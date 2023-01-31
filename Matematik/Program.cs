463b19b8d07ece1950a1a8bcd8016117a2e5a43c
Akiifunal <akif_unal@icloud.com>
2023-01-31 17:20:58 +0300

    Add project files.

---
+++ Matematik/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortıslem = new DortIslem();
+            dortıslem.Topla(5, 7);
+           
+        }
+    }
+}
+
