986a89b3674867075d9e1eb9146b14f99152291b
Akiifunal <111228884+Akiifunal@users.noreply.github.com>
2023-02-02 16:06:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortıslem = new DortIslem();
+            dortıslem.Topla(5, 7);
+            dortıslem.Cıkar(6, 8);
+            dortıslem.Carp(7, 8);
+        }
+    }
+}
+
